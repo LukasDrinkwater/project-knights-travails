2c88661b9b20e670e54b5d6f627107fc4525d9df
Lukas <lukasdrinkwater97@gmail.com>
2023-06-07 19:01:12 +0100

    started work on classes.js file

---
+++ webpack/src/classes.js
@@ -0,0 +1,9 @@
+class Datatree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+// each position with have nodes link to it.
+// the nodes will be other positions the knight can move to when
+// the knight is a given node
