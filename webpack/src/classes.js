74d9fe9cdf320d70654962fa80252ff183a985c1
Lukas <lukasdrinkwater97@gmail.com>
2023-06-08 21:30:23 +0100

    followed example of using Map() to make a graph

--- webpack/src/classes.js
+++ webpack/src/classes.js
@@ -1,9 +1,16 @@
-class Datatree {
-  constructor() {
-    this.root = null;
-  }
-}
+const possibleMoves = [];
+
+const adjacencyList = new Map();
 
 // each position with have nodes link to it.
 // the nodes will be other positions the knight can move to when
 // the knight is a given node
+
+//should be able to take a random start point A and end point B and find the
+//shortest path between them.
+
+// hardcoding generating offsets?
+//https://discord.com/channels/505093832157691914/690590001486102589/1075461413054726304
+
+// Wakana maybe usefull messages
+// https://discord.com/channels/505093832157691914/1049708420112072854
