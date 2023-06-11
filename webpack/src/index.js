cd845f0db44457b6c555db6ee699f5c9769635cd
Lukas <lukasdrinkwater97@gmail.com>
2023-06-11 21:18:04 +0100

    started and made progress on the findLegalMoves method in the Board class. Got it linking the legal moves to the this.move parameter

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { find } from "lodash";
 import "./styles.css";
 
 class Square {
@@ -37,13 +37,61 @@ class Board {
       }
     }
 
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        let square = boardArray[row][col];
+        this.findLegalMoves(square);
+      }
+    }
+
     return boardArray;
+  }
+  findLegalMoves(square) {
+    let board = this.boardArray;
+    let boardSize = 7;
+    let row = square.row;
+    let col = square.col;
+    let moveNum = 0;
+    let moveSets = [
+      [-1, -2],
+      [1, -2],
+      [2, -1],
+      [2, 1],
+      [1, 2],
+      [-1, 2],
+      [-2, 1],
+      [-2, -1],
+    ];
+
+    for (let moveset of moveSets) {
+      let moveRow = row + moveset[0];
+      let moveCol = col + moveset[1];
+      // let moveToSquare = board[moveRow][moveCol];
+
+      // find and store the square you are looking at so you can do squre.movex =
+      if (moveRow <= 7 && moveCol <= 7 && moveCol >= 0 && moveRow >= 0) {
+        let moveToSquare = board[moveRow][moveCol];
+        // link the current square square
+        square["move" + moveNum] = moveToSquare;
+        moveNum++;
+      }
+    }
+
+    //each moveset adds or takes away from the current x,y values of the square
+
+    //for each squre, check to see if + or - each move set makes the col/row
+    //greater than 7 or < 0
+
+    //if it doesnt get the square that the move set makes and add it to
+    //one of the this.movex proporties of the current square you are checking
+
+    //getting the current square will be similar to making them boardArray[i][j]
   }
 }
 
 let board = new Board();
 
-board.createGameboard();
+let gameboard = board.createGameboard();
 
 for (let i = 0; i < 8; i++) {
   let boardString = "";
@@ -53,4 +101,6 @@ for (let i = 0; i < 8; i++) {
   console.log(boardString);
 }
 
-// console.log(board.boardArray);
+console.log(board.boardArray);
+
+// console.log(board.findLegalMoves(gameboard));
